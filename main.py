80d0d365a9cccf0058470c14c6220f9d0ee4fc9f
timur <timurka.zavyalov@mail.ru>
2023-01-30 16:45:14 +0500

    git init

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from __init__ import list_dirs, root_directory
 concat_root_path: partial = partial(os.path.join, root_directory)
 make_directory: partial = partial(os.makedirs, exist_ok=True)
 
+
 for path_items in map(concat_root_path, list_dirs):
     make_directory(path_items)
